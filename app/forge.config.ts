d9a5fa681a648b30f00841bdf587bb8750fdeeca
陈薛强 <carlchenxq@qq.com>
2026-03-20 16:52:16 +0800

    fix: 修复 AutoUnpackNativesPlugin 初始化参数

--- app/forge.config.ts
+++ app/forge.config.ts
@@ -45,7 +45,7 @@ const config: ForgeConfig = {
     }),
   ],
   plugins: [
-    new AutoUnpackNativesPlugin(),
+    new AutoUnpackNativesPlugin({}),
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
       // If you are familiar with Vite configuration, it will look really familiar.
